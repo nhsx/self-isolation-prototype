aa2d69e6054a3e3dc32ddd9fb1c6f4c0395c5f53
Andreea Vlad <a.vlad@nhs.net>
2020-03-16 22:17:48 +0000

    push v2 again

--- app/routes.js
+++ app/routes.js
@@ -5,3 +5,15 @@ const router = express.Router();
 // Add your routes here - above the module.exports line
 
 module.exports = router;
+
+
+router.post('self-isolate', function(req,res){
+    var sisolate = req.session.data[sisolate]
+    if (sisolate == "yes"){
+        res.redirect('other-conditions')
+    }
+    else {
+        res.redirect('check-answers-no')
+    }
+
+})
