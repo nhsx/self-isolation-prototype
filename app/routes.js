59250d752eb2cd7edbba441d32f69695c762f5b6
Andreea Vlad <a.vlad@nhs.net>
2020-03-17 18:12:24 +0000

    push

--- app/routes.js
+++ app/routes.js
@@ -18,4 +18,14 @@ router.post('/updates-text', function(req,res){
 
 })
 
+router.post('live-alone1', function(req,res){
+    var contacted = req.session.data['live-alone123']
+    if (contacted == "yes"){
+        res.redirect('living-number')
+    }
+    if (contacted == "no"){
+        res.redirect('updates-text')
+    }
+})
+
 module.exports = router;
